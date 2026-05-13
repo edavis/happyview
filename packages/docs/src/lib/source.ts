1be26150787d2a1d87aa689d272a97cdbd433fa0
Trezy <tre@trezy.com>
2026-05-13 15:53:19 -0500

    docs: add a blog
    Signed-off-by: Trezy <tre@trezy.com>

--- packages/docs/src/lib/source.ts
+++ packages/docs/src/lib/source.ts
@@ -1,6 +1,10 @@
-import { docs } from 'collections/server';
+import { docs, blog } from 'collections/server';
 import { loader } from 'fumadocs-core/source';
 
 export const source = loader(docs.toFumadocsSource(), {
   baseUrl: '/',
+});
+
+export const blogSource = loader(blog.toFumadocsSource(), {
+  baseUrl: '/blog',
 });
